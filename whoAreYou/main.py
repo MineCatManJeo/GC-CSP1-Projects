523e9a99754d8394cba2153992330913322887aa
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2024-09-09 09:55:07 -0600

    Who are you added

---
+++ whoAreYou/main.py
@@ -0,0 +1,7 @@
+#Gabriel Crozier, Who are you
+
+stuff = input('Put your name, age, and favorite color with a space between each one: ')
+stuff = stuff.split()
+
+print(stuff[0])
+#print("So " + stuff[0].capitalize() + ", you're " + str(stuff[1]) + " right? I heard your favorite color was " + stuff(2) + ".")
