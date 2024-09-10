c6665dade8867f5b8db966403b54537703450b9d
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2024-09-09 21:04:34 -0600

    Changes to WhoAreYou python file Finished code Added complicated and normal mode Isn't rushed Input your name, age, and favorite color and spit out a line with those inside Simple version has 3 inputs and simply puts them in one output (no lists) Complicated version is a list which gets split To use complicated one you simply put in your name age and favorite color Restrictions include: You cannot have more than one word for the color It is prefered you use a number not a word for your age You may use your first, middle, and last name all with spaces in between In order to use it you type in your info like this: "first_name middle_name last_name age color" Middle name and last name aren't required Capitols aren't required

--- whoAreYou/main.py
+++ whoAreYou/main.py
@@ -1,7 +1,25 @@
 #Gabriel Crozier, Who are you
 
-stuff = input('Put your name, age, and favorite color with a space between each one: ')
-stuff = stuff.split()
+def complicate():
+    stuff = input('Put your name (Last names and Middle names are allowed, allowed with spaces as well), age, and favorite color (One Word) with a space between each one: ')
+    stuff = stuff.split()
+    if len(stuff[0:-2]) >= 2:
+        stuff[1] = stuff[1][0].upper() + '.'
 
-print(stuff[0])
-#print("So " + stuff[0].capitalize() + ", you're " + str(stuff[1]) + " right? I heard your favorite color was " + stuff(2) + ".")
+    print("So " + ' '.join(stuff[0:-2]).title() + ", you're " + str(stuff[-2]) + " right? I heard your favorite color was " + stuff[-1].lower() + "!")
+
+def easy():
+    name = input("Please input your name here --> ")
+    age = input("Please input your age here --> ")
+    color1 = input("Please input your favorite color here --> ")
+
+    print("So " + name.title() + ", you're " + str(age) + " right? I heard your favorite color was " + color1.lower() + "!")
+
+coolstuff = input("Complicated or not? (y/n) --> ")
+
+if coolstuff in ('y','n'):
+    if coolstuff == 'y':
+        complicate()
+    elif coolstuff == 'n':
+        easy()
+
