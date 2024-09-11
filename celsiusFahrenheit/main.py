2a0529702e0b03dece71687bc2d9176e61742977
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2024-09-11 09:47:33 -0600

    Added Celsius to Fahrenheit converter Asks user for celsius Prints fahrenheit + added sentence

---
+++ celsiusFahrenheit/main.py
@@ -0,0 +1,6 @@
+#Gabriel Crozier, Celsius to fahrenheit converter
+
+celsius = int(input('Enter temperature in celsius:\n'))
+fahrenheit = (celsius*9/5)+32
+
+print('When it is ' + str(celsius) + ' degrees Celsius it is ' + str(fahrenheit) + ' degrees Fahrenheit!')
