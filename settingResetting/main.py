d62be9ab5fae390a52bf4bc3726df548e36d1774
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2024-09-18 09:53:23 -0600

    Changed setting file Isn't done add more later NOT FINISHED

--- settingResetting/main.py
+++ settingResetting/main.py
@@ -1,1 +1,16 @@
 #Gabriel Crozier, setting & resetting
+
+# Variables
+staffA = 32
+studentA = 100
+guestA = 2 * studentA
+guestPTable = 12
+
+print(f"There are {staffA} staff attending, {studentA} students being awarded, {guestA} guests attending. {guestPTable} people can sit at each table!\n")
+# Changes
+studentA -= 1
+staffA -= 3
+guestA = 2 * studentA - 15
+staffA += 1
+print(f"There are {staffA} staff attending, {studentA} students being awarded, {guestA} guests attending. {guestPTable} people can sit at each table!\n")
+print((staffA+studentA+guestA)/guestPTable)
