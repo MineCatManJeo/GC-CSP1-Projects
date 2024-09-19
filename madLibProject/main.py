ef0ad8a776e2440a0a95f69b7218234101a9155e
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2024-09-19 09:52:59 -0600

    Added Madlib file Imported random Also added list Very unfinished

---
+++ madLibProject/main.py
@@ -0,0 +1,9 @@
+# Gabriel Crozier, Madlib Assignment
+
+import random as rand
+
+liOSent = ["One day while NAME was VERBING he encountered a NOUN. After looking carefully at the NOUN he realized it was VERBINGA.\
+            Soon after he asked it for it's favorite color. It said it's favorite color was COLOR. This made NAME feel really EMOTION \
+           and he mailed the NOUN to the PLACE.","After a long and difficult day NAME had enough. He decided to VERB while waiting for \
+            his lovely NOUN to come home. Sadly that wouldn't happen as he got a call. It turns out his lovely NOUN was VERBINGA. After \
+                a long pause NAME felt EMOTION. He immedietly ran to his lovely NOUN at the PLACE!","","",""]
