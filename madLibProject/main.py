653de47659c85e2ee5451a248600a6175e6c11ee
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2024-09-20 09:53:05 -0600

    Updated madlibs file Mostly done just need to change a few things and make the sentences more finished Added inputs for stuff Added more to the sentances

--- madLibProject/main.py
+++ madLibProject/main.py
@@ -2,8 +2,37 @@
 
 import random as rand
 
-liOSent = ["One day while NAME was VERBING he encountered a NOUN. After looking carefully at the NOUN he realized it was VERBINGA.\
-            Soon after he asked it for it's favorite color. It said it's favorite color was COLOR. This made NAME feel really EMOTION \
-           and he mailed the NOUN to the PLACE.","After a long and difficult day NAME had enough. He decided to VERB while waiting for \
-            his lovely NOUN to come home. Sadly that wouldn't happen as he got a call. It turns out his lovely NOUN was VERBINGA. After \
-                a long pause NAME felt EMOTION. He immedietly ran to his lovely NOUN at the PLACE!","","",""]
+name = input("Input a name:\n").capitalize()
+pronoun = input("Input this persons pronouns (he/she):\n").lower()
+verbA = input("Input a verb (ing something you can physically do):\n").lower()
+noun = input("Input a noun:\n").lower()
+verbB = input("Input a verb (ing something you can physically do):\n").lower()
+color = input("Input a color:\n").lower()
+emotion = input("Input an emotion:\n").lower()
+place = input("Input a place:\n").title()
+
+if pronoun.lower() == "he":
+    pronoun1 = "his"
+else:
+    pronoun1 = "her"
+
+
+vowels = ["a","e","i","o","u"]
+
+the_noun = "the " + noun
+
+if noun[1].lower() in vowels:
+    a_noun = "an " + noun
+else:
+    a_noun = "a " + noun
+
+liOSent = [f"One day while {name} was {verbA} {pronoun} encountered {a_noun}. After looking carefully at {the_noun} {pronoun} realized it was {verbB}. \
+Soon after {pronoun} asked it for it's favorite color. It said it's favorite color was {color}. This made {name} feel really {emotion} \
+and {pronoun} mailed {the_noun} to the {place}.",f"After a long and difficult day, {name} had enough. {pronoun.capitalize()} decided to begin {verbA} while waiting for \
+{pronoun1} lovely {noun} to come home. Sadly that wouldn't happen as {pronoun} just got a call. It turns out {pronoun1} lovely {noun} had broken the law because she was {verbB}. After \
+a long pause {name} felt {emotion}. {pronoun.capitalize()} immedietly ran across the {color} road to rescue {pronoun1} lovely {noun} from the {place}!",f"The {color} palace \
+was magnificant. {name} was the ruler of all and {pronoun1} biggest law prohibited {verbA}. Most of {pronoun1} citezens decided {pronoun} was a \
+bad ruler because of how {emotion} {pronoun} was. They all decided to hold {noun}s and throw them at {name} to retaliate. {name} was so sad \
+that {pronoun} was forced to be constantly {verbB} for the rest of {pronoun1} life."]
+
+print(liOSent[rand.randint(0,2)])
