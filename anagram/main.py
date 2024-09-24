cd9a849f6c48af34be0ab7a28aa800c3812a439f
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2024-09-23 23:14:57 -0600

    Updated anagram file Not 100% sure if I'm doing this right Added comments Made things easier to read The shuffling of the list works now even though I don't fully understand it Prints out a random arrangment of letters now I hope that's what I needed to do!!!

--- anagram/main.py
+++ anagram/main.py
@@ -1,13 +1,19 @@
 # Gabriel Crozier, anagram assignment for the raid
 
-
+# imports random
 import random
+
+# Variables
 name = input("What's your name:\n").lower()
-
 name1 = [x for x in name]
-
 nameList = []
 
+# Little thing to make view easier
+print('')
+
+# Shuffels the characters in the name around
 for i in range(5):
-    nameList.append(random.shuffle(name1))
-    print(nameList[-1])
+    random.shuffle(name1) # Shuffles the origional list
+    listVar = name1 # Assigns the new value to a variable
+    nameList.append(listVar) # Appends this to a list
+    print(''.join(nameList[-1]).title()) # Prints the full word
