f4de4bd75560e485db4be7ce864f3656035f3e95
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2024-10-02 09:41:51 -0600

    Added raidFile for functions Bare bones and might be rewritten later Attempting to use turtle graphics to acually draw the shape

---
+++ FunctionsFolder/functionsRaidAreaCalc/main.py
@@ -0,0 +1,16 @@
+#Gabriel Crozier, Radi for Functions
+
+def button(w,h,x,y):
+    t.goto(x,y)
+    t.seth(90)
+
+import turtle as t
+
+t.screensize(canvwidth=400, canvheight=400)
+button(1,1,200,200)
+
+
+t.getscreen()._root.mainloop()
+# figure our more at home
+# Goal: Get turtle to draw 5 buttons and have you click them for the turtle to draw if too complicated just do the number strat
+
