409d7b3abe6fb7744719616ab55d8edb7857cac9
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2024-10-02 22:25:26 -0600

    Updated cypher Allows for decoding of decoded text Allows for a decimal in the shift input (Not a good idea)

--- FunctionsFolder/secretCypher/main.py
+++ FunctionsFolder/secretCypher/main.py
@@ -6,7 +6,7 @@ def cypher(text,shift):
     for x in text: # once for EVERY letter in the string
         if x.isalpha(): # checks if it is in the alphabet
             if x.islower(): # checks for lovercase
-                ch = ord(x) + (shift%26) # if it is, shift it
+                ch = ord(x) + (int(shift%26)) # if it is, shift it
                 if ch > ord('z') and not ch < ord('a'):
                     ch -= 26 # makes sure if you make a num go above the 'z' value it wraps around
             elif x.isupper(): # same but for uppercase
@@ -17,7 +17,7 @@ def cypher(text,shift):
         sentence += chr(ch)
     return(sentence) # returns combined string
 
-listOfCommon = ['the ','and ','for ','how ','is '] # common words
+listOfCommon = [' the ',' and ',' for ',' how ',' is '] # common words
 
 yn = bool(int(input('Would you like to decode or encode?\nDecode: 1\nEncode: 0\n'))) # asks if you want to encode / decode
 if yn == False: # encode
@@ -27,14 +27,14 @@ if yn == False: # encode
 if yn == True: # decode
     txt = input("Input sentence:\n")
     listOfCommon.append(input('Input a word you think is in the cypher:\n'))
-    for i in range(25): # 25 for the num of letter in alphabet - 1
+    for i in range(26): # 26 for the num of letter in alphabet
         tf = False # resets value
         for li in listOfCommon: # once for each value in common words
-            if li in cypher(txt,i+1).lower(): # if its in the cipher
+            if li in cypher(txt,i).lower(): # if its in the cipher
                 tf = True # make this true
         if tf: # if its true
-            print('\n'+cypher(txt,i+1)[0:50], str(i+1) + '\n') # print a small part of the cipher
+            print('\n'+cypher(txt,i)[0:50], str(i+1) + '\n') # print a small part of the cipher
     num2 = int(input("\nWhich section seemed the most correct:\n")) # asks you which part looks right
-    print('\n'+cypher(txt,num2) + '\n') # prints full version of the part you though looked right
+    print('\n'+cypher(txt,num2-1) + '\n') # prints full version of the part you though looked right
 
     
