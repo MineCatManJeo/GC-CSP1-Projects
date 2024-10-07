992d21044e38860dd489fb245744f1678f35f1ce
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2024-10-07 09:29:07 -0600

    Added Comments folder Added Comments File

---
+++ commentsFolder/commentsPractice/main.py
@@ -0,0 +1,14 @@
+# Gabriel Crozier, comments practice
+
+# ------------------------------------------------------------------------------------
+# Name:         Input Name
+# Purpose:      To collect user info about name
+#
+# Author:       Gabriel Crozier
+#
+# Created:      10/7/2024
+# Lisence:      (None)
+#------------------------------------------------------------------------------------
+
+name = input('What is your name?: ') # Asks user for their name
+print("Hello",name) # prints their name
