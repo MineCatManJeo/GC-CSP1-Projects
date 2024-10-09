eafad6b75172c35bb52716003c1c325979a2ee97
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2024-10-09 09:53:51 -0600

    Added sdlc Folder Added sdlc file Added comments and set up outline

---
+++ sdlcFolder/randomNumGuessing/main.py
@@ -0,0 +1,32 @@
+# Gabriel Crozier, Number Guessing Game / SDLC Documentation
+
+#----------------------------------------------------------------------------------------------------
+#
+# Goals: The file needs to makes the user guess a random number between 1 - 10 and tell them if they are correct
+# Features: A random number generator between 1 - 10, user input allowing the user to guess random number, print statements telling them if they got the number correct
+# Required Resources: An ide to run the program, a computer, power
+#
+#----------------------------------------------------------------------------------------------------
+
+# Psuedocode
+
+"""
+
+
+
+
+
+"""
+
+# Implementation
+
+
+
+# Friend Comments:
+#
+#
+#
+#
+
+
+# Maintenence
