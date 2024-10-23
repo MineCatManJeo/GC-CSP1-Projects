7d88e5cdba4b2a91fc06effa826e53c5878b55ca
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2024-10-23 09:53:38 -0600

    Updated tic tac toe file Made the game work Need to make the computer smarter

--- TestFolderTicTacToe/ticTacToe/main.py
+++ TestFolderTicTacToe/ticTacToe/main.py
@@ -8,13 +8,14 @@ board = [
 xo = ['x']
 xo.insert(r.choice([0,1]),'o')
 
-def show_board(board):
+def show_board(board,turn):
+    print(f'{turn}')
     for i in range(3):
         print(board[0+(3*i)],board[1+(3*i)],board[2+(3*i)])
     print('')
 def user_place(board,xo):
     while True:
-        show_board(board)
+        show_board(board,'\nYour turn:')
         userNum = int(input(f'Input a number 1-9 (1 being the top left of board) to place your {xo[0].capitalize()}: '))-1
         if userNum > 8:
             userNum = 8
@@ -30,6 +31,7 @@ def comp_rand_place(board,xo):
         compNum = r.randint(0,8)
         if board[compNum] == '#':
             board[compNum] = xo[1]
+            show_board(board,'\nComputers Placement:')
             break
 def check_win(board): # NOTES MAYBE YOU CAN MAKE A % FOR EACH ROW / COLUMN LIKE row 1 has 33% empty and 66% x or smth then if the ratio is 100% it does the thing? Also if not I can try something like this for possdible other thing ALSO don't work on this project WEDNESDAY in class work on UML
     for i in range(3):
@@ -50,7 +52,13 @@ def check_win(board): # NOTES MAYBE YOU CAN MAKE A % FOR EACH ROW / COLUMN LIKE 
         print('It\'s a draw!')
         return True
 
-for i in range(3):
+if xo[0] == 'x':
     user_place(board,xo)
-if check_win(board):
-    show_board(board)
+while True:
+    comp_rand_place(board,xo)
+    if check_win(board):
+        break
+    user_place(board,xo)
+    if check_win(board):
+        break
+show_board(board,'\nFinal Board:')
