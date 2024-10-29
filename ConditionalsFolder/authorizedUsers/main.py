938cc3d50748d5b14742b2820432ebae32445f76
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2024-10-29 09:53:19 -0600

    Aded user file Not Done

---
+++ ConditionalsFolder/authorizedUsers/main.py
@@ -0,0 +1,4 @@
+# Gabriel Crozier, Authorized users
+
+users = [['admin','GabeTheGamer'],['user','JogleWessel123'],['user','Minecrafter45'],['user','BookW0rm1'],['user','fdsgoivfdsl'],
+         ['user','UCASRules1243'],['user','XxWataDogDoinxX'],['user','GamerBoy1984'],['user','FireCat5853'],['user','HenrySm1th'],['user','MarshmallowCat666'],['user','IamaUser'],['user'],['user']]
