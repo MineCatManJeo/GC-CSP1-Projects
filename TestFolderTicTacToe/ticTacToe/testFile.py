94df7dbe7c3817126cd0b021b03381760a65616b
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2024-11-06 09:54:08 -0700

    Added diagonal Function to specificly the test file, real addition is later

--- TestFolderTicTacToe/ticTacToe/testFile.py
+++ TestFolderTicTacToe/ticTacToe/testFile.py
@@ -1,29 +1,39 @@
 # This is my test file
-def find_percent(board):
+def find_percent(board,value): # NEED TO ADD FUNCTION FOR DIAGONAL
     rows = []
     columns = []
+    diagonals = []
     indexes = []
     percent = []
     for row in board:
-        rows.append(round(row.count('#')/0.03))
-        try: indexes.append(row.index('#'))
+        rows.append(round(row.count(value)/0.03))
+        try: indexes.append(row.index(value))
         except: indexes.append(-1)
     for col in range(3):
         column = ''
+        for row in board:
+            column += row[col]
+        columns.append(round(column.count(value)/0.03))
+        try: indexes.append(column.index(value))
+        except: indexes.append(-1)
+    for dig in range(2):
+        diagonal = ''
         for row in range(3):
-            column += board[row][col]
-        columns.append(round(column.count('#')/0.03))
-        try: indexes.append(column.index('#'))
+            diagonal += board[row][(row+dig)-(dig*2*(row+dig))] # needs to make the second loop time -1 and not times 0
+        print(diagonal)
+        diagonals.append(round(diagonal.count(value)/0.03))
+        try: indexes.append(diagonal.index(value))
         except: indexes.append(-1)
     percent.append(rows)
     percent.append(columns)
+    percent.append(diagonals)
     percent.append(indexes)
     return percent
 
 board = [
-    ['x','x','#'],
+    ['x','x','o'],
     ['o','#','#'],
-    ['o','x','x']
+    ['o','x','#']
 ]
 
-print(find_percent(board))
+print(find_percent(board,'#'))
