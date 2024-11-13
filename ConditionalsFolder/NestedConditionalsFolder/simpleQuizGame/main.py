6cd2652612ce1d53ac4a85f49ec5d4cf5d9267da
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2024-11-13 09:52:55 -0700

    Added simple quiz thingy Not done

---
+++ ConditionalsFolder/NestedConditionalsFolder/simpleQuizGame/main.py
@@ -0,0 +1,31 @@
+# Gabriel Crozier, Nested conditionals simple quiz game
+import time as t
+quzEasy = [
+    'What drops when you break a grass block with your fist?||A. Grass\nB. Dirt\nC. Grass Block\nD.Gravel',
+    'What pickaxe do you need to break obsidian?',
+    'How do you craft a furnace?',
+    'What do you press in order to attack a mob?',
+    'What do you use to trade with villagers?'
+]
+quzNormal = [
+    'What do you need to ride saddled pig in Minecraft?',
+    'What is the effect you get when you kill a pillager captain?',
+    'How do you craft an enchanting table?',
+    'Where does the end portal spawn in Minecraft?',
+    'How do you tame a cat in Minecraft?'
+]
+quzHard = [
+    'Which one of these items can you get as a tresure item from fishing?',
+    'How would you summon a horde of 25 zombies onto a player named MineCatManJoe using a command?',
+    'How much iron would you need for a full set of iron armor?',
+    'How do you get the star trader advancement in Minecraft?',
+    'What is the arbalistic advancement in Minecraft?'
+]
+
+def quiz():
+    print('\nWelcome to my epic most amazing quiz about Minecraft!!!!\n')
+    time = t.time()
+    diff = 1
+    for i in range(5):
+        pass
+quiz()
