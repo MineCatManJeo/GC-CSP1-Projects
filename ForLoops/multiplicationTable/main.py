93f1846ea04cd880e285963ac289c355b691fcb2
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2024-11-21 09:54:20 -0700

    Added multi talbe Not done Alsmot done

--- ForLoops/multiplicationTable/main.py
+++ ForLoops/multiplicationTable/main.py
@@ -1,14 +1,19 @@
 # Gabriel Crozier, Multiplication Table
 num = int(input('NUM: '))
 numList = list(range(num,num*12+num,num))
+numbList = []
 multiList = []
 for row in numList:
     addList = []
     for col in numList:
         addList.append(row*col)
     multiList.append(addList)
-for li in multiList:
-    for num1 in li:
-        num1 = str(num1)
-for li in multiList:
-    print(str(int(li[0]/num)) + ': ' + ' | '.join(li))
+formats = len(str(multiList[-1][-1]))
+for numb in numList:
+    numbList.append(str(numb) + '. ')
+print(' | '.join(numbList))
+for i in range(len(multiList)):
+    for j in range(len(multiList[i])):
+        multiList[i][j] = f'{str(multiList[i][j]):>{formats}}'
+    print(f"{numList[i]}. {' | '.join(multiList[i])}")
+    print(f"    {'-'*len(' | '.join(multiList[i]))}")
