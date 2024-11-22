c2dfd707b7e99eff952e13f9f61af372ad5c8f49
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2024-11-22 09:51:10 -0700

    FINISHED MULTI TABLE !!!! YES Too complex I forgor how it works It does tho YIPPEEE

--- ForLoops/multiplicationTable/main.py
+++ ForLoops/multiplicationTable/main.py
@@ -10,10 +10,11 @@ for row in numList:
     multiList.append(addList)
 formats = len(str(multiList[-1][-1]))
 for numb in numList:
-    numbList.append(str(numb) + '. ')
-print(' | '.join(numbList))
+    numbList.append(f'{numb:>{formats}}')
+print(' '*(len(numbList[-1].strip())+1) + '.  '.join(numbList) + '.')
 for i in range(len(multiList)):
     for j in range(len(multiList[i])):
         multiList[i][j] = f'{str(multiList[i][j]):>{formats}}'
-    print(f"{numList[i]}. {' | '.join(multiList[i])}")
-    print(f"    {'-'*len(' | '.join(multiList[i]))}")
+    print(f"{' '*(len(numbList[-1].strip())+2)}{'-'*len(' | '.join(multiList[0]))}")
+    print(f"{numList[i]:>{len(numbList[-1].strip())}}. {' | '.join(multiList[i])}")
+print(f"{' '*(len(numbList[-1].strip())+2)}{'-'*len(' | '.join(multiList[0]))}")
