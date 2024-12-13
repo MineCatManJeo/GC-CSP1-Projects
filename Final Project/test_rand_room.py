589c1c2bf9b457ccf0a3cdbf68c3853f68ff20e1
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2024-12-12 23:27:46 -0700

    Added simple walls system to test program

--- Final Project/test_rand_room.py
+++ Final Project/test_rand_room.py
@@ -7,28 +7,35 @@
 # ORRR prints grid with rooms labeled
 # (0,0) = first room
 import random
-cords = {}
 roomNum = 0
+cords = {0:{0:['Room ' + str(roomNum),random.choice(['l','r','u','d'])]}}
 current = [0,0]
 while True:
+    print("You can't move: " + cords[current[0]][current[1]][1])
     move = input('Input movement option (u:up, d:down, l:left, r:right): ')
-    if move == 'u':
-        current[1] += 1
-    elif move == 'd':
-        current[1] -= 1
-    elif move == 'l':
-        current[0] -= 1
-    elif move == 'r':
-        current[0] += 1
-    elif move == 's':
-        pass
+    print('\033c')
+    print('You tried to move: ' + move)
+    preRoom = current
+    if not move == cords[current[0]][current[1]][1]:
+        if move == 'u':
+            current[1] += 1
+        elif move == 'd':
+            current[1] -= 1
+        elif move == 'l':
+            current[0] -= 1
+        elif move == 'r':
+            current[0] += 1
+        elif move == 's':
+            pass
+        else:
+            break
     else:
-        break
+        print('You hit a wall!')
     if not current[0] in cords:
         cords[current[0]] = {}
     if not current[1] in cords[current[0]]:
-        cords[current[0]][current[1]] = ['Room ' + str(roomNum),random.choice(['a','b','c'])]
         roomNum += 1
-    print(cords[current[0]][current[1]])
+        cords[current[0]][current[1]] = ['Room ' + str(roomNum),random.choice(['l','r','u','d'])] # First is room, second is walls
+    print(cords[current[0]][current[1]][0])
 print(cords)
 # SO FAR! Creates a new generated room inside of a dictionary each time you explore!, Also gives the room a name and a random value
