b39ba2a1f13dafa5ab4c13946738167b051d4cdb
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2024-12-12 23:11:16 -0700

    Started test for final project

--- Final Project/test_rand_room.py
+++ Final Project/test_rand_room.py
@@ -6,11 +6,29 @@
 # Prints either list with rooms
 # ORRR prints grid with rooms labeled
 # (0,0) = first room
-cord = [0,0]
-x = 0
-print('Hi?')
+import random
+cords = {}
+roomNum = 0
+current = [0,0]
 while True:
-    x = int(input('What is happening'))
-    cord 
-    break
-print(cord)
+    move = input('Input movement option (u:up, d:down, l:left, r:right): ')
+    if move == 'u':
+        current[1] += 1
+    elif move == 'd':
+        current[1] -= 1
+    elif move == 'l':
+        current[0] -= 1
+    elif move == 'r':
+        current[0] += 1
+    elif move == 's':
+        pass
+    else:
+        break
+    if not current[0] in cords:
+        cords[current[0]] = {}
+    if not current[1] in cords[current[0]]:
+        cords[current[0]][current[1]] = ['Room ' + str(roomNum),random.choice(['a','b','c'])]
+        roomNum += 1
+    print(cords[current[0]][current[1]])
+print(cords)
+# SO FAR! Creates a new generated room inside of a dictionary each time you explore!, Also gives the room a name and a random value
