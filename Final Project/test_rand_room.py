cf6b4fc7794d0b3acf02f77503dc6db8904b68a4
MineCatManJeo <gabriel.crozier@ucas-edu.net>
2024-12-17 00:43:30 -0700

    Added inventory system Gave begginer items

--- Final Project/test_rand_room.py
+++ Final Project/test_rand_room.py
@@ -7,6 +7,12 @@
 # ORRR prints grid with rooms labeled
 # (0,0) = first room
 import random
+def preGameAssignment():
+    Presets = []
+    Presets.append(['to the leftL','to the rightR','in front ofU','behindD'])
+    Presets.append(['leftL','rightR','forwardsU','backwardsD'])
+    Presets.append(["you see a short hallway with a metal door at the end.","you see a rotting wooden door with 3 long slashes in the middle.","you see a wooden door that is almost prestine, only having a bit of dust which you brush off to see strong wood behind it.","farther than you would like to walk, you see a huge archway leading to another room.","you notice a crack in the wall which you think you can fit in.","there is a large intimidating strong door which you think might take a few shoves to open.","you find a small crawlspace you might be able to fit through.","you can see a heavy stone door, you just hope theres machinery to open it for you."])
+    return Presets
 # Function to add data to each room
 def data_room(data,room_cords, cur_locat):
     try:
@@ -40,8 +46,14 @@ def add_walls(pre_room, move):
     all4Walls.append(doorList)
     all4Walls.append(wallList)
     return all4Walls
+def check_inv(inv):
+    print('\033c')
+    print('Inventory:')
+    print(inv)
+    for slot in inv:
+        print(f'{slot} x{inv[slot]["amount"]}')
 # Print Your actions
-def action(rom_cords, cur_locat, preset):
+def action(rom_cords, cur_locat, preset, inv):
     print('You walk into the room and look around:')
     # Enemy (suddenly an orc... blah)
     # Large room detail (You find you are in a large puzzle room this is what you see)
@@ -49,7 +61,6 @@ def action(rom_cords, cur_locat, preset):
     # Exits (Room entrances / exits) (You see a door to the right)
     doorPlace = []
     doorPlace1 = []
-    print(cur_locat)
     for pos1 in rom_cords[cur_locat[0]][cur_locat[1]][1][0]:
         for pos2 in preset[0]:
             if pos1.upper() == pos2[-1]:
@@ -62,20 +73,22 @@ def action(rom_cords, cur_locat, preset):
     # Actions (What would you like to do)
     print('\n')
     actionOps = input("""What would you like to do:
-1. Move
-2.
-3.
-4.
+1. Check Inventory
+2. Check Stats
+3. Combat needs to be first before anything else
+4. Search / move / other options
 ----> """)
     try: actionOps = int(actionOps)
     except: 
-        print('User did not input a number, try again not coded yet!')
+        print('User did not input a number, try again not coded yet!') # if user fails retype everything and clear terminal
         return True, True
     # Combat
     # Item
     # Search
     # Move
     if actionOps == 1:
+        check_inv(inv)
+    if actionOps == 3:
         breakLoop, move, cur_locat = movement(preset, doorPlace1, rom_cords, cur_locat)
     return breakLoop, move, cur_locat
 # Movement
@@ -84,10 +97,12 @@ def movement(preset,doorPlace1, rom_cords, cur_locat):
     for move_option in preset[1]:
         for door in doorPlace1:
             if move_option[-1].lower() == door:
-                print(f'Move {move_option[0:-1]} ({move_option[-1]})')
+                print(f'Move {move_option[0:-1]} ({move_option[-1]})') # ATTENTYION NEEDS TO ADD A RETURN TO PREVIOUS ROOM (P or B)
     move = input('Input movement option (u:up, d:down, l:left, r:right): ').lower()
     print('\033c')
-    print('You moved [insert move here]')
+    for option in preset[1]:
+        if move.upper() == option[-1]:
+            print(f'You moved {option[0:-1].lower()}!')
     if not move == rom_cords[cur_locat[0]][cur_locat[1]][1][1]:
         if move == 'u':
             cur_locat[1] += 1
@@ -101,29 +116,69 @@ def movement(preset,doorPlace1, rom_cords, cur_locat):
     else:
         print('Not a valid movement option!')
         return True, move, cur_locat
-
-
-# In Game text preset
-Presets = []
-Presets.append(['to the leftL','to the rightR','in front ofU','behindD'])
-Presets.append(['leftL','rightR','forwardsU','backwardsD'])
-Presets.append(["you see a short hallway with a metal door at the end.","you see a rotting wooden door with 3 long slashes in the middle.","you see a wooden door that is almost prestine, only having a bit of dust which you brush off to see strong wood behind it.","farther than you would like to walk, you see a huge archway leading to another room.","you notice a crack in the wall which you think you can fit in.","there is a large intimidating strong door which you think might take a few shoves to open.","you find a small crawlspace you might be able to fit through.","you can see a heavy stone door, you just hope theres machinery to open it for you."]) # Doors
+# Item Management
+def item(inv, action, items = 0, amount = 1, dataItem = {}):
+    # options for data: damage, coming soon
+    # 2 options, delete, add (works with multiple items / you can add 2 arrows to 4 arrows)
+    if action == 'add':
+        if items != 0:
+            if not items in inv:
+                inv[items] = {}
+                inv[items]['amount'] = 0
+                inv[items]['dataItem'] = dataItem
+            if dataItem == inv[items]['dataItem']:
+                inv[items]['amount'] += amount
+            else:
+                items += '|' + str(len(inv))
+                inv[items] = {}
+                inv[items]['amount'] = amount
+            inv[items]['dataItem'] = dataItem
+        else: pass
+        # maybe for random weopons     
+    elif action == 'addType': # Adds a specific type of tool / trinket to the inventory (Not done needs presets)
+        if not items in inv:
+            inv[items] = {}
+            inv[items]['amount'] = 0
+            inv[items]['dataItem'] = dataItem
+        if dataItem == inv[items]['dataItem']:
+            inv[items]['amount'] += amount
+        else:
+            items += '|' + str(len(inv))
+            inv[items] = {}
+            inv[items]['amount'] = amount
+        inv[items]['dataItem'] = dataItem
+    elif action == 'del':
+        try:
+            inv[items]['amount'] -= amount
+            if inv[items]['amount'] == 0:
+                inv.pop(items)
+        except:
+            print('Error, item does not exist')
+    return inv
 # Starting values
+Presets = preGameAssignment()
 print('\033c')
 roomNum = 0
 preRoom = 0
 current = [0,0]
 cords = {}
+inv = {}
+# Starting Gear
+inv = item(inv, 'add', 'Copper Sword', 1, {'damage':15})
+inv = item(inv, 'add', 'Chainmail Pants', 1, {'armor':3})
+inv = item(inv, 'add', 'Leather Chestplate', 1, {'armor':4})
+inv = item(inv, 'add', 'Small Health Potion', 3, {'healing':25})
+# add type trinket (extra speed, extra health, speed boots, blah, blah)
+# Optional gold
 cords, roomNum = assign_room(cords, current, roomNum)
+
 while True:
-    action_vars = action(cords, current, Presets)
+    action_vars = action(cords, current, Presets, inv) # Instead, make return values like 'move', 'check_inv' and then if is move then get these values
     preRoom = current
-    print(action_vars)
     if action_vars[0]:
         break
     current = action_vars[2]
     cords, roomNum = assign_room(cords, current, roomNum, action_vars[1], preRoom)
 print(cords)
-# SO FAR! Creates a new generated room inside of a dictionary each time you explore!, Also gives the room a name and a random value
-# need to make more changable, do this by using functions and stuff
-# Also needs to have more complex wall system that works with more than one wall.
+# Almost finished movement sysstem, doesnt work very well with other systems but thats a later me problem
+# Inventory system started, not even close to done
